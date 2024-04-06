a87d36de3d85269ed71057346fec8c59c3fe52c0
Sujeet Gund <sujeetgund@gmail.com>
2024-04-06 22:30:47 +0530

    Variables in JS

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 2310023
+let accountName = 'Sujeet Gund'
+var accountCity = 'Pune'
+let accountStatus;
+
+
+// accountId = 10021
+
+
+accountName = 'Sujeet'
+accountCity = 'Barshi'
+
+console.table([accountId, accountName, accountCity, accountStatus])
+
