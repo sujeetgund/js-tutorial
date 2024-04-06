13d9f8ee598f1f899dd6420ea6112fa7167858be
Sujeet Gund <sujeetgund@gmail.com>
2024-04-06 23:16:19 +0530

    Datatype Conversion in JS

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,38 @@
+let score = "40abc"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+// ** String to Number Conversion **
+
+// If score = null, then valueInNumber = 0
+// If score = undefined, then valueInNumber = NaN
+// If score = "Sujeet", then valueInNumber = NaN
+// If score = true, then valueInNumber = 1
+// If score = false, then valueInNumber = 0
+
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn)
+
+// ** Conversion to Boolean **
+
+// If isLoggedIn = 1, then booleanIsLoggedIn = true
+// If isLoggedIn = 0, then booleanIsLoggedIn = false
+// If isLoggedIn = "", then booleanIsLoggedIn = false
+// If isLoggedIn = "Sujeet", then booleanIsLoggedIn = true
+
+
+let someNumber = 20
+
+let stringNumber = String(someNumber)
+
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
