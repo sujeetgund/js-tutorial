97a4bf4b9ab2c2bacd1b9eb534051945c4ee0c79
Sujeet Gund <sujeetgund@gmail.com>
2024-04-06 22:53:20 +0530

    Datatypes in JS

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict"; // Treat all JS code as newer version
+
+// ** Primitive Datatypes **
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value or empty value
+// undefined => declared but not initialized
+// symbol => unique
+
+
+// Object
+
+console.log(typeof null)  // object
+console.log(typeof undefined)  // undefined
