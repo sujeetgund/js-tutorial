7360791295f9f7cc2eecdd1288cd42482329703d
Sujeet Gund <sujeetgund@gmail.com>
2024-04-06 23:43:01 +0530

    Datatypes Conversion Opeations in JS

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -36,3 +36,35 @@ let stringNumber = String(someNumber)
 
 // console.log(stringNumber)
 // console.log(typeof stringNumber)
+
+
+// ************************** Operations **************************
+let value = 4
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2**2)
+// console.log(2%2)
+
+let str1 = "Hello,"
+let str2 = " Sujeet"
+// console.log(str1 + str2)
+
+// console.log(1 + "2") => "12"
+// console.log("1" + 2) => "12"
+// console.log("1" + 2 + 2)  => "122"
+// console.log(1 + 2 + "2")  => "32"
+
+// console.log(true) => true
+// console.log(+true) => 1
+// console.log(true+) => Error
+
+// console.log(+"") => 0
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2+2;
